065fe0fa0601567329ea5133083d0d57792a39bd
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2021-07-21 22:46:47 +0530

    Reverse Array

--- src/main/java/array/ReverseAnArray.java
+++ src/main/java/array/ReverseAnArray.java
@@ -11,62 +11,58 @@ import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class ReverseAnArray {
-    public static void main(String[] args) {
-       int[] arr = new int[3];
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array element you want to reverse: ");
-       for(int i=0;i< arr.length;i++){
-           arr[i]=sc.nextInt();
-       }
-        System.out.println("Array is:- ");
-        for(int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-        System.out.println("Reverse of the above array is:- ");
-        for(int i=arr.length-1;i>=0 ;i--){
-            System.out.print(arr[i]+" ");
+    //*** In GFG ***
+
+    static void revereseArray(int arr[],
+                             int start, int end)
+    {
+        int temp;
+
+        while (start < end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 
 
-    //*** In GFG ***
-
-//    static void rvereseArray(int arr[],
-//                             int start, int end)
+    //**OR
+    
+//static void revereseArray(int arr[],
+//                          int start, int end)
 //    {
 //        int temp;
-//
-//        while (start < end)
-//        {
-//            temp = arr[start];
-//            arr[start] = arr[end];
-//            arr[end] = temp;
-//            start++;
-//            end--;
-//        }
+//        if (start >= end)
+//            return;
+//        temp = arr[start];
+//        arr[start] = arr[end];
+//        arr[end] = temp;
+//        revereseArray(arr, start+1, end-1);
 //    }
-//
-//    /* Utility that prints out an
-//    array on a line */
-//    static void printArray(int arr[],
-//                           int size)
-//    {
-//        for (int i = 0; i < size; i++)
-//            System.out.print(arr[i] + " ");
-//
-//        System.out.println();
-//    }
-//
-//    // Driver code
-//    public static void main(String args[]) {
-//
-//        int arr[] = {1, 2, 3, 4, 5, 6};
-//        printArray(arr, 6);
-//        rvereseArray(arr, 0, 5);
-//        System.out.print("Reversed array is \n");
-//        printArray(arr, 6);
-//
-//    }
+
+    /* Utility that prints out an
+    array on a line */
+    static void printArray(int arr[],
+                           int size)
+    {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        printArray(arr, arr.length);
+        revereseArray(arr, 0, arr.length-1);
+        System.out.println("Reversed array is ");
+        printArray(arr, arr.length);
+
+    }
 
 }
